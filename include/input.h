2345458a2b9b133231419335cc1e68e25d2a9192
Nur Fajar Hidayatullah <fajar.nur.h@pm.me>
2024-06-13 07:13:28 +0700

    Add collusion for all cell

--- include/input.h
+++ include/input.h
@@ -6,41 +6,43 @@
 Vector2 mousePos;
 static int filled = 0;
 
+// TEMPORARY ARRAY FOR TEST
+#define CELL_CAP 9
+Rectangle rectList[CELL_CAP];
+int colorState[CELL_CAP];
+
 void handleInput(Board *board);
 void drawCollision(Board *board);
-
 #ifdef INPUT_IMPLEMENTATION
 
 void
 handleInput(Board *board)
 {
-
     mousePos = GetMousePosition();
-    Rectangle rect = (Rectangle){
-        board->startOffset.x,
-        board->startOffset.y,
-        board->cell,
-        board->cell
-    };
 
-    if (CheckCollisionPointRec(mousePos, rect)) filled = 1;
-    else filled = 0;
+    for (int i = 0; i < CELL_CAP; i++) {
+        rectList[i].x = board->line/2 + board->startOffset.x + board->cell*(i%3);
+        rectList[i].y = board->line/2 + board->startOffset.y + board->cell*(i/3);
+        rectList[i].width = board->cell - board->line;
+        rectList[i].height = board->cell - board->line;
+    }
+
+    for (int i = 0; i < CELL_CAP; i++) {
+        if (CheckCollisionPointRec(mousePos, rectList[i])) colorState[i] = 1;
+        else colorState[i] = 0;
+    }
 }
 
 void
 drawCollision(Board *board)
 {
-    Rectangle rect = (Rectangle){
-        board->startOffset.x + board->line/2,
-        board->startOffset.y + board->line/2,
-        board->cell - board->line,
-        board->cell - board->line
-    };
+    for (int i = 0; i < CELL_CAP; i++) {
+        DrawRectangleRec(rectList[i], BLACK0);
 
-    if (filled == 1)
-        DrawRectangleRec(rect, BLACK1);
-    else
-        DrawRectangleRec(rect, BLACK0);
+        if (colorState[i]) {
+            DrawRectangleRec(rectList[i], BLACK1);
+        }
+    }
 }
 
 #endif
